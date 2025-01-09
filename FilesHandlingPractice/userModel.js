7930534fd3d3eb7b5c32c56d8be6b7e82b0765fe
Sonali Kumari Keshri <89269598+Sonali-keshri@users.noreply.github.com>
2025-01-09 16:01:34 +0530

    Add changes

---
+++ FilesHandlingPractice/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/basicCrudProj')
+
+
+const userSchema =  mongoose.Schema({
+    name: String,
+    userName: String,
+    email: String,
+})
+
+
+module.exports = mongoose.model('user', userSchema)
